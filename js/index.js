1840f1e225717306bcc0868ff936ed627f87b33b
BORJA FARIÑA GONZÁLEZ <BORJA.FARIA@aula.local>
2019-01-10 19:42:07 +0100

    previsualizar imagen

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $("#file").change((e) =>{
+        
+        let reader=new FileReader();
+        reader.readAsDataURL(e.target.files[0]);  // para leer la primera posicion del fichero
+
+        reader.onload = function(){   // cuando reader ejecute el fichero lanza la funcion
+
+            $("#imagen").attr("src",reader.result);
+        }
+    });
+});
